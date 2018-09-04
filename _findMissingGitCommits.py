2e11cc7a6b34b68425e871f5c09382c7d1f9db46
Lisa Rushworth <lisa.rushworth@windstream.com>
2018-09-04 17:27:52 -0400

    Including function to send e-mail notice when repos with missing commits are identified

--- _findMissingGitCommits.py
+++ _findMissingGitCommits.py
@@ -3,11 +3,22 @@
 ################################################################################
 # Root from which to search for git repos
 strSearchRoot = '/bhs00p10/Scripts'
+# String used when repo does not appear to be valid
+strRepoError = "Invalid GIT repository"
+strSenderAddress = "devnull@windstream.com"
+strRecipientAddresses = "lisa.rushworth@windstream.com"
+strMailRelay = "mail.windstream.com"
+iMailRelayPort = 25
 
+# Import everything we need
 import os
 from subprocess import check_output
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText 
 
-strRepoError = "Invalid GIT repository"
+
+strReposWithMissingCommits = [];
 
 for dirName, subdirList, fileList in os.walk(strSearchRoot):
 	if dirName.endswith("/.git"):
@@ -26,4 +37,26 @@ for dirName, subdirList, fileList in os.walk(strSearchRoot):
 		elif strResults == strRepoError:
 			print('Invalid git repository at %s' % dirToProcess)
 		elif len(strResults) >= 1:
-			print('%s has uncommitted changes' % dirToProcess)
+#			print('%s has uncommitted changes' % dirToProcess)
+			strReposWithMissingCommits.append(dirToProcess)
+
+if len(strReposWithMissingCommits) > 1:
+	strMessageContent = ""
+	for strRepo in strReposWithMissingCommits:
+		strMessageContent = strMessageContent + "<tr><td>" + strRepo + "</td></tr>"
+		print('%s has uncommited changes' % strRepo)
+	
+	mimeMsg = MIMEMultipart()
+	mimeMsg['From'] = strSenderAddress
+	mimeMsg['To'] = strRecipientAddresses
+	mimeMsg['Subject'] = "GIT Repos With Outstanding Commits"
+	mimeMsg.attach(MIMEText(strMessageContent,'html'))
+
+	smtpServer = smtplib.SMTP(strMailRelay, iMailRelayPort)
+	#smtpServer.set_debuglevel(True)
+	smtpServer.ehlo()
+	smtpServer.starttls()
+	smtpServer.ehlo()
+	strMessageText = mimeMsg.as_string()
+	smtpServer.sendmail(strSenderAddress, strRecipientAddresses, strMessageText)
+
